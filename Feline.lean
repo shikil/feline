e92bca41aecac877d506bfc4b7fbbb01a9b28c3e
Tim <{ID}+{username}@users.noreply.github.com>
2026-03-20 21:28:15 +0800

    add LeanCopilot library

--- Feline.lean
+++ Feline.lean
@@ -1,3 +1,7 @@
 -- This module serves as the root of the `Feline` library.
 -- Import modules here that should be built as part of the library.
 import Feline.Basic
+import LeanCopilot  
+
+example (a b c : Nat) : a + b + c = a + c + b := by
+  suggest_tactics
