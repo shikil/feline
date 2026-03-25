84acd7747b3948f83b11c25e83914c7d35a81984
Tim <{ID}+{username}@users.noreply.github.com>
2026-03-25 20:55:48 +0800

    add Chapter 3.1 Positive number example

--- Feline.lean
+++ Feline.lean
@@ -1,6 +1,8 @@
 -- This module serves as the root of the `Feline` library.
 -- Import modules here that should be built as part of the library.
 import Feline.Basic
+import Feline.Pos
+
 import LeanCopilot  
 
 example (a b c : Nat) : a + b + c = a + c + b := by
---
+++ Feline/Pos.lean
@@ -0,0 +1,33 @@
+inductive Pos : Type where
+  | one : Pos
+  | succ : Pos → Pos
+
+def Pos.plus : Pos → Pos → Pos
+  | Pos.one, k => Pos.succ k
+  | Pos.succ n, k => Pos.succ (n.plus k)
+
+instance : Add Pos where
+  add := Pos.plus
+
+def Pos.toNat : Pos → Nat
+  | Pos.one => 1
+  | Pos.succ n => n.toNat + 1
+
+instance : ToString Pos where
+  toString x := toString (x.toNat)
+
+def posToString (atTop : Bool) (p : Pos) : String :=
+  let paren s := if atTop then s else "(" ++ s ++ ")"
+  match p with
+  | Pos.one => "Pos.one"
+  | Pos.succ n => paren s!"Pos.succ {posToString false n}"
+
+def Pos.mul : Pos → Pos → Pos
+  | Pos.one, k => k
+  | Pos.succ n, k => n.mul k + k
+
+instance : Mul Pos where
+  mul := Pos.mul
+
+def seven : Pos := Pos.succ (Pos.succ (Pos.succ (Pos.succ (Pos.succ (Pos.succ Pos.one)))))
+#eval s!"There are {seven}"
