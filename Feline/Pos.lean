6318254524d84323b71bacaad90cd0cd77ef8f1e
Tim <{ID}+{username}@users.noreply.github.com>
2026-03-28 20:39:33 +0800

    simply Pos Instance

--- Feline/Pos.lean
+++ Feline/Pos.lean
@@ -2,39 +2,34 @@ inductive Pos : Type where
   | one : Pos
   | succ : Pos → Pos
 
-def Pos.plus : Pos → Pos → Pos
-  | Pos.one, k => Pos.succ k
-  | Pos.succ n, k => Pos.succ (n.plus k)
-
 instance : Add Pos where
-  add := Pos.plus
-
-def addNatPos : Nat → Pos → Pos
-  | 0, p => p
-  | n + 1, p => Pos.succ (addNatPos n p)
-
-def addPosNat : Pos → Nat → Pos
-  | p, 0 => p
-  | p, n + 1 => Pos.succ (addPosNat p n)
+  add := let rec posPlus : Pos → Pos → Pos
+    | Pos.one, k => Pos.succ k
+    | Pos.succ n, k => Pos.succ (posPlus n k)
+    posPlus
 
 instance : HAdd Nat Pos Pos where
-  hAdd := addNatPos
+  hAdd := let rec addNatPos : Nat → Pos → Pos
+    | 0, p => p
+    | n + 1, p => Pos.succ (addNatPos n p)
+    addNatPos
 
 instance : HAdd Pos Nat Pos where
-  hAdd := addPosNat
-
-def hashPos : Pos → UInt64
-  | Pos.one => 0
-  | Pos.succ n => mixHash 1 (hashPos n)
+  hAdd := let rec addPosNat : Pos → Nat → Pos
+    | p, 0 => p
+    | p, n + 1 => Pos.succ (addPosNat p n)
+    addPosNat
 
 instance : Hashable Pos where
-  hash := hashPos
+  hash := let rec hashPos : Pos → UInt64
+    | Pos.one => 0
+    | Pos.succ n => mixHash 1 (hashPos n)
+    hashPos
 
-instance : OfNat Pos (n + 1) where
-  ofNat :=
-    let rec natPlusOne : Nat → Pos
-      | 0 => Pos.one
-      | k + 1 => Pos.succ (natPlusOne k)
+  instance : OfNat Pos (n + 1) where
+  ofNat := let rec natPlusOne : Nat → Pos
+    | 0 => Pos.one
+    | k + 1 => Pos.succ (natPlusOne k)
     natPlusOne n
 
 def Pos.toNat : Pos → Nat
@@ -50,12 +45,11 @@ def posToString (atTop : Bool) (p : Pos) : String :=
   | Pos.one => "Pos.one"
   | Pos.succ n => paren s!"Pos.succ {posToString false n}"
 
-def Pos.mul : Pos → Pos → Pos
-  | Pos.one, k => k
-  | Pos.succ n, k => n.mul k + k
-
 instance : Mul Pos where
-  mul := Pos.mul
+  mul := let rec posMul : Pos → Pos → Pos
+    | Pos.one, k => k
+    | Pos.succ n, k => posMul n k + k
+    posMul
 
 def seven : Pos := Pos.succ (Pos.succ (Pos.succ (Pos.succ (Pos.succ (Pos.succ Pos.one)))))
 #eval s!"There are {seven}"
